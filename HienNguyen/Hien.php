c855a9913e53b3f6917267eb226ee0a1f1ac1caa
tmchiennguyen <h.nguyen.ptit@gmail.com>
2014-01-15 17:18:14 +0700

    Lan 2

--- HienNguyen/Hien.php
+++ HienNguyen/Hien.php
@@ -1,10 +1,23 @@
 <?php
-$cars=array("Volvo","BMW","Toyota");
-$arrlength=count($cars);
+$c1 = "Red";
+   $c2 = array("yamaha", "Toyota", "bla bla");
+echo "tui di xe" .$c2[0].". Anh di xe".$c2[1].$c2[2]." aaaaaaaaaaaa";
+print "<br>Alo mot hai ba bon"."<br> alo alo";
+print "<br>tui di xe".$c2[0].". Anh di xe".$c2[1].$c2[2]." aaaaaaaaaaaa";
+echo "<br>Alo mot hai ba bon"."<br> alo alo";
+echo "<h2> ABC $c1  </h2>";
 
-for($x=0;$x<$arrlength;$x++)
-{
-    echo $cars[$x];
-    echo "<br>";
+
+$a1=1.68126;
+$a2=2+3*2;
+$a3=3E-4;
+
+ $a4=$a1+$a2;
+echo "$a4 abc xyz $a1 + $a2<br>";
+echo "<br>".strpos("abcdef", "ef");
+echo "<br>". strlen("asdkajsldka<br>");
+
+foreach($c2 as $c){
+echo "Xe $c <br>";
 }
 ?>
