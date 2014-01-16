27ea68dafb7880d4ef56604e29a61e05b8a4d8c6
NgocMen <ngocmennguyen.gl@gmail.com>
2014-01-16 14:04:24 +0700

    init

---
+++ nmen/BT1_Forms/Form1.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+<form action="welcome.php" method="post">
+    UserName: <input type="text" name="username"><br>
+    E-mail: <input type="text" name="email"><br>
+    Password: <input type ="text" name ="pass"><br>
+    Password Comfirm :<input type ="text" name ="pass2"><br>
+    <input type="submit">
+</form>
+
+</body>
+</html>
---
+++ nmen/BT1_Forms/Form2.php
@@ -0,0 +1,15 @@
+<html xmlns="http://www.w3.org/1999/html">
+<body>
+
+<form action="bt.php" method="post">
+    UserName:<input type = "text" name = "UserName">   <br>
+    Email   :<input type ="text" name = "Email">   <br>
+    Website :<input type ="text" name ="Website">      <br>
+    Comment :<input textarea name ="Comment" rows = "8" cols ="50"></textarea>    <br>
+    Gender: <input type ="radio" name ="Gender"  value ="Female"> Female
+    <input type ="radio" name = "Gender"     value=" Male">Male    <br>
+     <input type="submit">
+</form>
+
+</body>
+</html>
