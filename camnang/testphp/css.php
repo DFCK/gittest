1c9e3670240856a5aba3f686e3a6ec86478ae3bf
CamNang269 <camnang269@gmail.com>
2014-01-22 16:54:29 +0700

    testphp1

--- camnang/testphp/css.php
+++ camnang/testphp/css.php
@@ -1,5 +1,6 @@
 <HTML>
 <HEAD>
+
     <TITLE>Hyperlink</TITLE>
     <STYLE TYPE='text/css'>
         A:link { Font-family:Arial,Helvetica; size:13pt; color:blue; Text-Decoration:none }
@@ -7,6 +8,7 @@
         A:active {Font-size:13pt; color:black; Text-Decoration:none }
         A:hover { Color:red; Text-Decoration:Underline }
     </STYLE>
+
 </HEAD>
 <BODY>
 <A HREF=″http://www.tinhte.vn/″>Trang tin tuc</A>
@@ -27,10 +29,44 @@
     </div>
 </ul>
 
-<a ><IMG SRC="hinhanh/184314_533192116703038_1072519955_n.jpg" WIDTH="403" HEIGHT="403" border="0"></a>
+<a ><img SRC="hinhanh/184314_533192116703038_1072519955_n.jpg" WIDTH="403" HEIGHT="403" border="0"></a>
+
+    <div style="color: #CC0000 ; width: 100px; height: 100px">
+       <p>Nguyen Thi Cam Nang</p>
+    </div>
+
+
+<a href="#">Link 1</a>
+<a href="#">Link 2</a>
+<a href="#">Link 3</a>
+
+<a href="#">Link 1</a>
+<a href="#" style="position: fixed; top: 100px; right: 100px;">Link 2</a>
+<a href="#">Link 3</a>
+
+<a href="#">Link 1</a>
+<a href="#" style=" position: relative; left: 100">Link 2</a>
+<a href="#">Link 3</a>
+
+<div style="width: 200px; margin: 0 auto; position: relative">
+    <a href="#">Link 1</a>
+    <a href="#" style="position: absolute; bottom: 0px; right: 0px">Link 2</a>
+    <a href="#">Link 3</a>
+</div>
+
+<div style="height: 280px; margin: 20px; position: relative;">
+
+    <div style="background-color: blue; height: 200px; left: 0px; position: absolute; top: 0px; width: 200px;z-index: 1"></div>
+
+    <div style="background-color: black; height: 200px; left: 40px; position: absolute; top: 40px; width: 200px;z-index: 3"></div>
+
+    <div style="background-color: red; height: 200px; left: 80px; position: absolute; top: 80px; width: 200px;z-index: 5"></div>
+
+</div>
 </BODY>
 </HTML>
 <?php
+
 //A:link: Thiết lập các thuộc tính của hyperlink khi chưa chọn tới.
 //A:visited: Thiết lập các thuộc tính của hyperlink khi đã chọn tới.
 //A:active: Thiết lập thuộc tính của hyperlink ngay sau khi được kích hoạt, tương tự như A:visited.
--- camnang/testphp/html.php
+++ camnang/testphp/html.php
@@ -1,12 +1,27 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title></title>
+    <title>Hello</title>
+    <link href="design.css" rel="stylesheet" type="text/css">
+    <!--    <style type="text/css">-->
+<!---->
+<!--      div{ color: blue}-->
+<!--      p{ color: yellow}-->
+<!--        title{ color: yellow}-->
+<!--    </style>-->
+
 </head>
 <body>
 
 <marquee align="center" direction="left" height="100" scrollamount="2" width="100%">
    Nguyen Thi Cam Nang
 </marquee>
+<form>
+    <div>
+        Nguyen thi cam nang
+    </div>
+    <p>nguyen thi cam nang</p>
+
+</form>
 </body>
 </html>
